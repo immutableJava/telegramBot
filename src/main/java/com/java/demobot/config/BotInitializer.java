3e86bb7f42a6e555d8d4eb465b8b5a01a1006607
Huawei <meelo.k@mail.ru>
2023-01-20 17:59:42 +0400

    primitive bot

---
+++ src/main/java/com/java/demobot/config/BotInitializer.java
@@ -0,0 +1,22 @@
+package com.java.demobot.config;
+
+import com.java.demobot.service.TelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class BotInitializer {
+    @Autowired
+    TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+    }
+}
